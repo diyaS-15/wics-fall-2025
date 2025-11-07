5a7a1457433ac288b7b983384cf23551d117a598
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2025-11-07 14:56:57 -0700

    Next.js set up

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-10">
+      <h1 className="text-red-500 text-4xl text-center">Hello This is Our Homepage!</h1>
+    </div>
+  );
+}
