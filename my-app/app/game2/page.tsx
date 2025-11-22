e55dc8592464c8b179fb29c49f7173577547fc52
sanguy06 <103820641+sanguy06@users.noreply.github.com>
2025-11-22 11:08:14 -0700

    Centering game on the page

--- my-app/app/game2/page.tsx
+++ my-app/app/game2/page.tsx
@@ -1,12 +1,14 @@
 export default function Game2() {
     return (
+      <div className="flex items-center justify-center h-screen">
     <iframe
-      src="/game2/index.html"
+      src="game2/index.html"
       width="800"
       height="600"
       style={{ border: "none" }}
       title="Godot Game"
       allowFullScreen
     />
+    </div>
   );
 }
