d3addcf1665a3c98902b880c9747ec82cd12bf61
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2025-11-22 15:40:41 -0700

    homepage styling

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     // m-10 refers to margin of 10 pixels, so this means that there's 10px of space around this entire div object 
     // the main div object contains all of our code, so anything visible on our app will have that space on the outside 
 
-    <div className="p-10 font-jersey bg-[#e2cfea] h-300">
+    <div className="p-10 font-jersey h-300 bg-gradient-to-br from-[#151023] to-purple-700 text-[#D19BFF]">
 
       {/* notice how the commenting style changed here -> whenever we work inside of our elements like div/ 
       things that are going to appear on our screen we comment in this way */}
@@ -19,43 +19,43 @@ export default function Home() {
       For styling, write className="..." with ... being the content using the TailwindCSS documentation reference */}
 
       {/* !!! ------- PLAY AROUND WITH THE STYLING HERE & CUSTOMIZE IT ---------- !!! */}
-      <h1 className="text-5xl text-center mt-8 mb-20">GirlsCodeGames.org</h1>
+      <h1 className="text-5xl text-center mb-15 ">GirlsCodeGames.org</h1>
 
       {/* grid is a layout system in CSS, the purpose here is to have 2 games per row */}
-      <div className="grid grid-cols-2 gap-4 my-10">
-        <div className="p-10 border text-center">
-
-          {/* Link element used to link to another page inside the application. 
-          Notice in the app folder, there is a subfolder called game1, which has it's own page.tsx 
-          In our main app folder, page.tsx is what we see when first open application - this is at the root (/)
-
-          Since we want to naivgate to a different page (ex: /game1), we would create a new folder with that name, 
-          which has it's own page.tsx similar to the one at root (which is what we're on right now) 
-
-          After setting up the subfolder, to link our current page.tsx to the other one, we use the Link element. 
-          Inside the start of Link element, href directs where to go, the href should be the same name of your subfolder 
-          just with a / at the start. */}
-
-          <Link href="/game1">Game1 - Diya Demo. Click on this and it will take you to Game1 page. </Link>
-        </div>
-        <div className="p-10 border text-center">
+      <div className="grid grid-cols-2 gap-4 my-10 text-2xl">
+        <div className="p-15 border text-center">
           {/* !!! ------- CREATE YOUR OWN SUBPAGE HERE + ADD LINK -------- !!!  */}
-          <Link href="/game2">Game2 - Sara+Natalie Demo. Click on this and it will take you to Game2 page. </Link>
+          <Link href="/game2">Game2 - Sara+Natalie Demo </Link>
         </div>
-        <div className="p-10 border text-center">
+        <div className="p-15 border text-center">
           {/* !!! ------- CREATE YOUR OWN SUBPAGE HERE + ADD LINK -------- !!!  */}
-          <Link href="/game3">Game3 - Ishani+Izzy Demo. Click on this and it will take you to Game3 page. </Link>
+          <Link href="/game3">Game3 - Ishani+Izzy Demo </Link>
         </div>
-        <div className="p-10 border text-center">
+        <div className="p-15 border text-center">
           {/* !!! ------- CREATE YOUR OWN SUBPAGE HERE + ADD LINK -------- !!!  */}
-          <Link href="/game5">Game5 - Samhita Demo. Click on this and it will take you to Game5 page. </Link>
+          <Link href="/game5">Game5 - Samhita Demo </Link>
         </div>
-        <div className="p-10 border text-center">
+        <div className="p-15 border text-center">
           {/* !!! ------- CREATE YOUR OWN SUBPAGE HERE + ADD LINK -------- !!!  */}
-          <Link href="/game4">Game4 - Tanvi Demo. Click on this and it will take you to Game4 page. </Link>
+          <Link href="/game4">Game4 - Tanvi Demo </Link>
         </div>
-          {/* COPY PASTE THE ABOVE ELEMENTS FOR MORE PAGES */}
+          {/* COPY PASTE THE ABOVE ELEMENTS FOR MORE PAGES 
+        (/*
+          <div className="p-10 border text-center">
 
+* Link element used to link to another page inside the application. 
+Notice in the app folder, there is a subfolder called game1, which has it's own page.tsx 
+In our main app folder, page.tsx is what we see when first open application - this is at the root (/)
+
+Since we want to naivgate to a different page (ex: /game1), we would create a new folder with that name, 
+which has it's own page.tsx similar to the one at root (which is what we're on right now) 
+
+After setting up the subfolder, to link our current page.tsx to the other one, we use the Link element. 
+Inside the start of Link element, href directs where to go, the href should be the same name of your subfolder 
+just with a / at the start. * 
+
+<Link href="/game1">Game1 - Diya Demo. Click on this and it will take you to Game1 page. </Link>
+</div> */}
       </div>
     </div>
   );
